6267380ddeea6ca58b2cb8d8d34cd8bf75ca852a
Markus John <johm@quyo.de>
2015-11-21 09:26:04 +0100

    Upgrade to MPIR 2.7.2

--- xmpir_ModdedForMpir.NET/gen-and-build-win.bat
+++ xmpir_ModdedForMpir.NET/gen-and-build-win.bat
@@ -1,10 +1,8 @@
 @rem Edit paths below to match your setup
 @rem
-@SET VCVARS32PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
-@rem @SET VCVARS32PATH="C:\Program Files\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
+@SET VCVARS32PATH="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 @rem
-@SET VCVARS64PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
-@rem @SET VCVARS64PATH="C:\Program Files\Microsoft Visual Studio 12.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
+@SET VCVARS64PATH="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
 @rem
 @SET PHPEXEPATH="d:\portable\php\php-5.6.5-nts-Win32-VC11-x86\php.exe"
 @rem
